50de94f70f20281796902066df22f142e1450f60
Ubuntu <ubuntu@ip-172-31-41-210.us-east-2.compute.internal>
2018-06-23 03:32:47 +0000

    Updated port number to 8080

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-
+const port = 8080
 
 app.use(cors());
 
 require('./routes')(app);
 
-app.listen(4000, (error)=>{
+app.listen(port, (error)=>{
     if(error)
     {
-        console.log("Can not able to start server on port 4000 : " + error);
+        console.log("Can not able to start server on port" + port + " : " + error);
     }
     else
     {
-        console.log('listening on 4000')
+        console.log('listening on '+ port)
     }
 })
 
